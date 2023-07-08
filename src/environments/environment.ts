c640c5927cca7a9f8aa27f8aeaf8a0edaab9658e
Muaaz Yusuf <m.yusuf@moddakir.com>
2023-07-08 19:55:22 +0300

    reverted base url for docker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    base_url: 'https://localhost:7026/',
+    base_url: 'http://localhost:8080/',
     api_prefix: 'api/',
     api_version: 'v1/',
     white_listed_Domains: ['localhost'],
